df5808b0cd818523fe4ab25f546fd1c14bc67ea4
liontecozmin <cozminlionte@gmail.com>
2021-04-14 17:21:40 +0000

    brokenRule query

--- src/main/java/com/tfo/maintenance/dao/alerts/AlertsRepository.java
+++ src/main/java/com/tfo/maintenance/dao/alerts/AlertsRepository.java
@@ -23,9 +23,43 @@ public interface AlertsRepository extends JpaRepository<Alerts, UID> {
     Collection<Alerts> getReport0(Date startDate, Date endDate);
 
     @Query("SELECT a FROM Alerts a WHERE a.tenantId like %?1 AND DAY(a.alertDate)=4 AND a.alertDate BETWEEN ?2 AND ?3 order by a.alertDate DESC")
-    Collection<Alerts> getReport1(String tenantId,Date startDate, Date endDate);
+    Collection<Alerts> getReport1(String tenantId, Date startDate, Date endDate);
 
     @Query("SELECT a FROM Alerts a WHERE (a.tenantId like %?1 OR a.tenantId like %?2) AND DAY(a.alertDate)=4 AND a.alertDate BETWEEN ?3 AND ?4 order by a.alertDate DESC")
-    Collection<Alerts> getReport2(String tenantId1, String tenantId2,Date startDate, Date endDate);
+    Collection<Alerts> getReport2(String tenantId1, String tenantId2, Date startDate, Date endDate);
+
+    @Query(value = "SELECT  DISTINCT b.AlertId,\n" +
+            "        t.CorrelationId,\n" +
+            "        (b.imo),\n" +
+            "        [CreatedAt],\n" +
+            "        [TrackingItemId],\n" +
+            "        b.Description,\n" +
+            "        t.BranchId,\n" +
+            "        REPLACE(t.TenantId,'00000000-0000-0000-0000-000000000001', 'Germany') as TenantId,\n" +
+            "    t.EventId\n" +
+            "\t\t--,m.TfoBranchName\n" +
+            "\t\t,v.Name\n" +
+            "\t\t,v.Imo\n" +
+            "\t\t,v.DeparturePort\n" +
+            "\t\t,v.DepartureDate\n" +
+            "\t\t,v.ArrivalPort\n" +:wq
+
+            "\t\t,v.ArrivalDate\n" +
+            "\t\t,v.VoyageNumber\n" +
+            "\t\t,v.Owner\n" +
+            "\t\t,v.BeneficialOwner\n" +
+            "\t\t,v.Flag\n" +
+            "\t\t,v.LastMovementDate\n" +
+            "\t\t,b.BrokenRules\n" +
+            "\n" +
+            "    FROM [Conpend.Database.Core].[dbo].[VesselTrackingItem] AS v\n" +
+            "    WITH (NOLOCK)\n" +
+            "    inner JOIN [Conpend.Database.Core].[dbo].[TrackingItem] AS t\n" +
+            "    on v.TrackingItemId=t.Id\n" +
+            "    inner join  [Microsoft.Integration.Trafinas].[vesselmonitor].[Alert] as b\n" +
+            "    on v.Imo=b.IMO and v.TrackingItemId=t.Id\n" +
+            "    where CorrelationId is not null\n" +
+            "    order by AlertId\"")
+    Collection<Alerts> getBrokenRuleReport(String tenantId1, String tenantId2, Date startDate, Date endDate);
 
 }
